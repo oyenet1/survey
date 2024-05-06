733ffcc0af6cd989d6f7ab23adb4af885f4f0f2b
oyenet1 <bowofadeoyerinde@gmail.com>
2024-05-06 15:32:23 +0100

    new work done with email ok

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Survey extends Model
 {
     use HasFactory;
+
+    function lga(): BelongsTo
+    {
+        return $this->belongsTo(Lga::class);
+    }
+
+    function enumerator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public static function booted()
+    {
+        self::created(function ($survey) {
+            $survey->update(['lga_id' => $survey->enumerator->lga_id]);
+        });
+    }
 }
