072314eb3eb17aab75a5b267d2b2197e0c84370e
oyenet1 <bowofadeoyerinde@gmail.com>
2024-05-06 15:19:16 +0100

    new work done with email ok

--- app/Models/Lga.php
+++ app/Models/Lga.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Lga extends Model
 {
     use HasFactory;
+
+    function state(): BelongsTo
+    {
+        return $this->belongsTo(State::class);
+    }
 }
