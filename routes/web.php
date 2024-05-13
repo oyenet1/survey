56660e12975d0eb770965b2166ae92f948fb781f
oyenet1 <bowofadeoyerinde@gmail.com>
2024-05-13 11:20:21 +0100

    notifications

--- routes/web.php
+++ routes/web.php
@@ -14,20 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
-        return view('welcome');
-    });
-    Route::get('/survey/{state:name}', \App\Livewire\SurveyPage::class)->name('survey');
-
-    Route::get('/dashboard', function () {
-        return view('welcome');
-    })->name('dashboard');
-
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 import preset from "./vendor/filament/support/tailwind.config.preset";
 
+/** @type {import('tailwindcss').Config} */
 export default {
     presets: [preset],
     content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
         "./app/Filament/**/*.php",
         "./resources/views/**/*.blade.php",
         "./vendor/filament/**/*.blade.php",
     ],
+
     theme: {
         extend: {
             colors: {
@@ -15,4 +22,23 @@ export default {
             },
         },
     },
+
+    // plugins: [forms],
 };
+
+// export default {
+//     presets: [preset],
+//     content: [
+//         "./app/Filament/**/*.php",
+//         "./resources/views/**/*.blade.php",
+//         "./vendor/filament/**/*.blade.php",
+//     ],
+//     theme: {
+//         extend: {
+//             colors: {
+//                 primary: "#73C3E8",
+//                 secondary: "#243c5a",
+//             },
+//         },
+//     },
+// };
