281aca4c06f6ba994d16a5a40354fd8d75e2d816
oyenet1 <bowofadeoyerinde@gmail.com>
2024-05-13 14:27:16 +0100

    notifications

--- app/Livewire/SurveyPage.php
+++ app/Livewire/SurveyPage.php
@@ -279,7 +279,7 @@ class SurveyPage extends Component implements HasForms
                         ])->columns(2)
                 ])
                     ->submitAction(new HtmlString('<button type="submit"
-                        class="px-6 py-2 mx-auto my-3 font-medium text-white rounded bg-[#73C3E8] hover:bg-secondary hover:text-black max-w-max">Submit</button>'))
+                        class="px-6 py-2 mx-auto my-3 font-medium text-white rounded bg-primary hover:bg-secondary hover:text-black max-w-max">Submit</button>'))
                     ->columnSpanFull(),
 
             ])->columns(2)
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Lga;
+use App\Models\State;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +17,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->nullable()->unique();
+            $table->string('ward')->nullable();
+            $table->string('role')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->foreignIdFor(Lga::class)->nullable()->constrained()->nullOnDelete();
+            $table->foreignIdFor(State::class)->nullable()->constrained()->cascadeOnDelete();
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
